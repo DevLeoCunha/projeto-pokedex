9767c9758d78087014b46b0749f9c9abc0297bd1
Leonardo Cunha <121736215+DevLeoCunha@users.noreply.github.com>
2024-08-27 04:17:12 +0100

    ...

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-const fetchPokemon = (pokemon) => {
+const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__number');
+const PokemonImage = document.querySelector('.pokemon__image');
 
-    const APIResponse = fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+const fetchPokemon = async (pokemon) => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const data = await APIResponse.json();
+    return data;
+}
+
+const renderPokemon = async (pokemon) => {
+
+    const data = await fetchPokemon(pokemon);
+    
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
 
 }
+
+renderPokemon('')
