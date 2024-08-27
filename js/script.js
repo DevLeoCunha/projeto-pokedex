8bf8bf4d8ed49ac42ef4d5ed5a8224fa0de170bf
Leonardo Cunha <121736215+DevLeoCunha@users.noreply.github.com>
2024-08-27 04:56:44 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,20 +8,24 @@ const input = document.querySelector('.input__search');
 
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    
+    if (APIResponse.status === 200) {
     const data = await APIResponse.json();
     return data;
+    }
 }
 
 const renderPokemon = async (pokemon) => {
 
     const data = await fetchPokemon(pokemon);
-    
+
+    if (data) {   
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
     pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
 
     input.value = '';
-
+    }
 }
 
 form.addEventListener('submit', (event) => {
